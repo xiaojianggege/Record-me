e6d9b6ca00fdc40134a1cda1e266661e6c325068
xiaojianggege <2804149747@qq.com>
2020-09-13 10:51:10 +0800

    J 9.13-1

--- miniprogram/components/footerbar/footerbar.js
+++ miniprogram/components/footerbar/footerbar.js
@@ -11,13 +11,36 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    isShowOne: true,
+    isShowTwo: false,
+    personalUrlActive: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    handleActiveOne() {
+      this.setData({
+        isShowOne: true,
+        personalUrlActive: false
 
+      })
+    },
+    handleActiveTwo() {
+      this.setData({
+        isShowOne: false,
+        isShowTwo: true,
+        personalUrlActive: false
+
+      })
+    },
+    handleActiveThree() {
+      this.setData({
+        isShowOne: false,
+        isShowTwo: false,
+        personalUrlActive: true
+      })
+    }
   }
 })
