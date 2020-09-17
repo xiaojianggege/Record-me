27162957bcd9175aeb085029318288b193f15ab2
xiaojianggege <2804149747@qq.com>
2020-09-17 11:11:50 +0800

    J 9.17-1

--- miniprogram/pages/note/note.js
+++ miniprogram/pages/note/note.js
@@ -1,18 +1,52 @@
 // miniprogram/pages/note/note.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    show: false,
+    minDate: new Date(2020,8,1).getTime(),
+    maxDate: new Date(2025,10,5).getTime(),
+    currentDate: new Date(),
+    value: '',
+    formatter(type, value) {
+    if (type === 'year') {
+      return `${value}年`;
+    } else if (type === 'month') {
+      return `${value}月`;
+    }
+    return value;
   },
-
+  },
+  showPopup() {
+    this.setData({
+      show: true
+    })
+  },
+  confirm(val) {
+    let time = new Date(val.detail)
+    let value = this.formatTime(time)
+    this.setData({
+      show: false,
+      value
+    })
+  },
+  // 格式化时间
+  formatTime(val) {
+    let year = val.getFullYear()
+    let month = val.getMonth() + 1
+    if (month >= 1 && month <= 9) { month = `0${month}` }
+    return `${year}-${month}`
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let value = new Date()
+    value = this.formatTime(value)
+    this.setData({
+      value
+    })
   },
 
   /**
